d73ed989e71070fc70a3c0fcbe890314c43c229e
lelenjsjs <a20202079@pucp.edu.pe>
2025-11-18 13:59:41 -0500

    ll

---
+++ FrontEnd_v2/KawkiWebBusiness/VentasBO.cs
@@ -0,0 +1,50 @@
+﻿using KawkiWebBusiness.KawkiWebWSUsuarios;
+using KawkiWebBusiness.KawkiWebWSVentas;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KawkiWebBusiness
+{
+    public class VentasBO
+    {
+        private VentasClient clienteSOAP;
+
+        public VentasBO()
+        {
+            this.clienteSOAP = new VentasClient();
+        }
+
+        public int InsertarVenta(KawkiWebWSVentas.usuariosDTO usuario, double total,
+                                 descuentosDTO descuento, redesSocialesDTO redSocial)
+        {
+            return this.clienteSOAP.insertarVenta(usuario, total, descuento, redSocial);
+        }
+
+        public ventasDTO ObtenerPorIdVenta(int ventaId)
+        {
+            return this.clienteSOAP.obtenerPorIdVenta(ventaId);
+        }
+
+        public IList<ventasDTO> ListarTodosVenta()
+        {
+            return this.clienteSOAP.listarTodosVenta();
+        }
+
+        public int ModificarVenta(int ventaId, KawkiWebWSVentas.usuariosDTO usuario,
+                                  double total, descuentosDTO descuento,
+                                  redesSocialesDTO redSocial, bool esValida)
+        {
+            return this.clienteSOAP.modificarVenta(
+                ventaId, usuario, total, descuento, redSocial, esValida
+            );
+        }
+
+        // Si decides habilitar eliminarVenta en tu servicio Java:
+        // public int EliminarVenta(int ventaId)
+        // {
+        //     return this.clienteSOAP.eliminarVenta(ventaId);
+        // }
+    }
+}
